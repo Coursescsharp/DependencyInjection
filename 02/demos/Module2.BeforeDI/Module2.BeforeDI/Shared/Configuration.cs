7caa2e6214ee34d83eb22488e483b20e0de2e155
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2023-07-26 09:53:41 +0200

    Introducing dependency injection

--- 02/demos/Module2.BeforeDI/Module2.BeforeDI/Shared/Configuration.cs
+++ 02/demos/Module2.BeforeDI/Module2.BeforeDI/Shared/Configuration.cs
@@ -4,6 +4,7 @@ public class Configuration
     // We will deal with passing in configuration in a better way in a future module
     // For now hardcoding the values is enough to practice with the concepts from this module
 
-    public string SourceCsvPath => @"c:\src-pluralsight\product-input.csv";
-    public string TargetCsvPath => @"c:\src-pluralsight\product-output.csv";
+    public string SourceCsvPath => @"C:\Users\ernestoantonio.rodri\Dev\c-sharp-10-dependency-injection\02\demos\Module2.BeforeDI\Module2.BeforeDI\product-input.csv";
+    
+    public string TargetCsvPath => @"C:\Users\ernestoantonio.rodri\Dev\c-sharp-10-dependency-injection\02\demos\Module2.BeforeDI\Module2.BeforeDI\product-output.csv";
 }
